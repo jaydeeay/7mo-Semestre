942c1017653cd3b302ac4b532582b29627551c7c
jaydeeay <jarroyavep@outlook.com>
2023-01-27 17:35:25 -0500

    Sistemas 27/01
    Se empieza con la actividad relacionada a respuestas de HTTPS y creación de db propia en github(JSONSV)

--- Assets/Sistemas Interactivos Distribuidos/Scripts/RestApiManager.cs
+++ Assets/Sistemas Interactivos Distribuidos/Scripts/RestApiManager.cs
@@ -6,23 +6,35 @@ using UnityEngine.UI;
 
 public class RestApiManager : MonoBehaviour
 {
-    [SerializeField] private RawImage YourRawImage;
+    [SerializeField] private List<RawImage> YourRawImage;
+    [SerializeField] private List<string> UserDeck = new List<string>();
+    [SerializeField] private int UserId = 1;
+    [SerializeField] private string ServerApiPath = "https://my-json-server.typicode.com/jaydeeay/JSONSV";
+    [SerializeField] private string RickAndoMortyApi = "https://rickandmortyapi.com/api";
     public int[] cards;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     public void GetCharacterClick() 
     {
-        StartCoroutine(GetCharacters());
-    }
-
-    IEnumerator GetCharacters()
+        StartCoroutine(GetCharacter(1));
+    }    
+    public void GetPlayerInfoClick() 
     {
-        UnityWebRequest www = UnityWebRequest.Get("https://rickandmortyapi.com/api/character/");
+        StartCoroutine(GetPlayerInfo());
+
+        int i =0;
+        foreach (string item in UserDeck)
+        {
+            StartCoroutine(DownloadImage(item, i));
+            Debug.Log(i);
+            i++;
+        }
+    }   
+
+    IEnumerator GetPlayerInfo()
+    {
+        string url = ServerApiPath + "/users/" + UserId;
+        Debug.Log(url);
+        UnityWebRequest www = UnityWebRequest.Get(url);
         yield return www.Send();
 
         if (www.isNetworkError)
@@ -34,21 +46,21 @@ public class RestApiManager : MonoBehaviour
             //Debug.log(www.GetResponseHeader("content-type"));
 
             // Show results as text
-            Debug.Log(www.downloadHandler.text);
+            //Debug.Log(www.downloadHandler.text);
 
 
 
-            if(www.responseCode == 200)
+            if (www.responseCode == 200)
             {
-                CharacterList characters = JsonUtility.FromJson<CharacterList>(www.downloadHandler.text);
-                Debug.Log(characters.info.count);
+                UserData user = JsonUtility.FromJson<UserData>(www.downloadHandler.text);
 
-                foreach(Character character in characters.results)
+                Debug.Log("Name:" + user.name);
+                
+
+                foreach (int item in user.deck)
                 {
-                    Debug.Log("Name:" + character.name);
-                    Debug.Log("Image:" + character.image);
-                    StartCoroutine(DownloadImage(character.image));
-                    break;
+                    //Debug.Log(item);
+                    StartCoroutine(GetCharacter(item));
                 }
             }
             else
@@ -63,16 +75,52 @@ public class RestApiManager : MonoBehaviour
             byte[] results = www.downloadHandler.data;
         }
     }
-
-    IEnumerator DownloadImage(string MediaUrl)
+    IEnumerator GetCharacter(int Id)
     {
-        UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl);
-        yield return request.SendWebRequest();
-        if (request.isNetworkError || request.isHttpError)
-            Debug.Log(request.error);
-        else YourRawImage.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+        UnityWebRequest www = UnityWebRequest.Get(RickAndoMortyApi + "/character/" + Id);
+        yield return www.Send();
+
+        if (www.isNetworkError)
+        {
+            Debug.Log("NETWORK ERROR:" + www.error);
+        }
+        else
+        {
+            //Debug.log(www.GetResponseHeader("content-type"));
+
+            // Show results as text
+            //Debug.Log(www.downloadHandler.text);
+
+
+
+            if (www.responseCode == 200)
+            {
+                Character character = JsonUtility.FromJson<Character>(www.downloadHandler.text);
+                UserDeck.Add(character.image);
+            }
+            else
+            {
+                string mensaje = "Status:" + www.responseCode;
+                mensaje += "\ncontent-type:" + www.GetResponseHeader("content-type");
+                mensaje += "\nError:" + www.error;
+                Debug.Log(mensaje);
+            }
+
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+        }
     }
 
+    IEnumerator DownloadImage(string url, int place)
+    {
+        yield return new WaitForSeconds(0.5f);
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
+        yield return request.SendWebRequest();
+        
+        if (request.isNetworkError || request.isHttpError)
+            Debug.Log(request.error);
+        else YourRawImage[place].texture = ((DownloadHandlerTexture)request.downloadHandler).texture;                
+    }
 }
 
 [System.Serializable]
@@ -98,4 +146,11 @@ public class Character
     public string name;
     public string specie;
     public string image;
+}
+
+public class UserData
+{
+    public int id;
+    public string name;
+    public int[] deck;
 }
